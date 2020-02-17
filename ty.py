f12c77ab3e160a3a8232d28484f01170aa7d8c38
wjw55555 <wujingwei55555@gmail.com>
2020-02-17 10:39:57 -0700

    test file status

---
+++ ty.py
@@ -0,0 +1,18 @@
+original = "waterfall"
+
+result = ""
+
+for char in original:
+    print("Getting Char:",char)
+    result = char + result
+    print("result now is ",result)
+    print("end of loop")
+    print()
+    
+print(result)
+print("length of original is: ",len(original))
+print("length of result is: ",len(result))
+
+
+
+
